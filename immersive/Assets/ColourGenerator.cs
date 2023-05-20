207098c00c0679a1520fe1cfa6f99535406eaa48
Liam Arbuckle <liam@skinetics.tech>
2023-05-20 15:11:01 +1000

    💼🪻 ↝ Adding high quality/realistic texture component of the generator. See https://github.com/Signal-K/sytizen/issues/18 && https://skinetics.notion.site/fc9cae9c027f4e22b276303aa8306e36?v=6c35d6d36a0344c8b8ba24f22f0f1d76&pvs=4

---
+++ immersive/Assets/ColourGenerator.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColourGenerator {
+
+    ColourSettings settings;
+    Texture2D texture;
+    const int textureResolution = 50;
+
+    public void UpdateSettings(ColourSettings settings)
+    {
+        this.settings = settings;
+        if (texture == null || texture.height != settings.biomeColourSettings.biomes.Length) {
+            texture = new Texture2D(textureResolution, settings.biomeColourSettings.biomes.Length);
+        }
+    }
+
+    public void UpdateElevation(MinMax elevationMinMax) {
+        settings.planetMaterial.SetVector("_elevationMinMax", new Vector4(elevationMinMax.Min, elevationMinMax.Max));
+    }
+
+    public BiomePercentFromPoint(Vector3 pointOnUnitSphere) {
+        float heightPercent = (pointOnUnitSphere.y + 1 / 2f);
+        float biomeIndex = 0;
+        int numBiomes = settings.biomeColourSettings.biomes.length;
+
+        for (int i = 0; i < numBiomes; i++) {
+            if (settings.biomeColourSettings.biomes[i].startHeight < heightPercent) {
+                biomeIndex = i;
+            } else {
+                break;
+            }
+        }
+
+        return biomeIndex / Mathf.Max(1, numBiomes - 1);
+    }
+
+    public void UpdateColours()
+    {
+        Color[] colours = new Color[texture.width * texture.height];
+        int colourIndex = 0;
+        foreach (var biome in settings.biomeColourSettings.biomes) {
+            for (int i = 0; i < textureResolution; i++) {
+                Color gradientCol = biome.gradient.Evaluate(i / (textureResolution - 1f));
+                Color tintCol = biome.tint;
+                colours[colourIndex] = gradientCol * (1 - biome.tintPercent) + tintCol * biome.tintPercent;
+                colourIndex++;
+            }
+        }
+        texture.SetPixels(colours);
+        texture.Apply();
+        settings.planetMaterial.SetTexture("_texture", texture);
+    }
+}
