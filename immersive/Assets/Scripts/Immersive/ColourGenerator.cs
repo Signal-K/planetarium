872d38f7a52328648384fcda2f0fa9310592dd07
Liam Arbuckle <liam@skinetics.tech>
2023-05-24 17:14:22 +1000

    😵‍💫👩🏻‍🦰 ↝ Adding plastic/ky & Mars type high-res scenes and updating colour generator with beginnings of height-based biome generation

--- immersive/Assets/Scripts/Immersive/ColourGenerator.cs
+++ immersive/Assets/Scripts/Immersive/ColourGenerator.cs
@@ -8,26 +8,43 @@ public class ColourGenerator {
     Texture2D texture;
     const int textureResolution = 50;
 
-    public void UpdateSettings(ColourSettings settings)
-    {
+    public void UpdateSettings(ColourSettings settings) {
         this.settings = settings;
-        if (texture == null)
-        {
-            texture = new Texture2D(textureResolution, 1);
+        if (texture == null || texture.height != settings.biomeColourSettings.biomes.Length) {
+            texture = new Texture2D(textureResolution, 1);// settings.biomeColourSettings.biomes.Length);
         }
     }
 
-    public void UpdateElevation(MinMax elevationMinMax)
-    {
+    public void UpdateElevation(MinMax elevationMinMax) {
         settings.planetMaterial.SetVector("_elevationMinMax", new Vector4(elevationMinMax.Min, elevationMinMax.Max));
     }
 
-    public void UpdateColours()
-    {
-        Color[] colours = new Color[textureResolution];
-        for (int i = 0; i < textureResolution; i++)
-        {
-            colours[i] = settings.gradient.Evaluate(i / (textureResolution - 1f));
+    public float BiomePercentFromPoint(Vector3 pointOnUnitSphere) {
+        float heightPercent = (pointOnUnitSphere.y + 1) / 2f;
+        float biomeIndex = 0;
+        int numBiomes = settings.biomeColourSettings.biomes.Length;
+
+        for (int i = 0; i < numBiomes; i++) {
+            if (settings.biomeColourSettings.biomes[i].startHeight < heightPercent) {
+                biomeIndex = i;
+            } else {
+                break;
+            }
+        }
+
+        return biomeIndex / Mathf.Max(1, numBiomes - 1)
+    }
+
+    public void UpdateColours() {
+        Color[] colours = new Color[texture.width * texture.height];
+        int colourIndex = 0;
+        foreach (var biome in settings.biomeColourSettings.biomes) {
+            for (int i = 0; i < textureResolution; i++) {
+                Color gradientCol = biome.gradient.Evaluate(i / (textureResolution - 1f));
+                Color tintCol = biome.tint;
+                colours[colourIndex] = gradientCol * ( 1 - biome.tintPercent ) + tintCol * biome.tintPercent;
+                colourIndex++;
+            }
         }
         texture.SetPixels(colours);
         texture.Apply();
--- immersive/Assets/Scripts/Immersive/Planet.cs
+++ immersive/Assets/Scripts/Immersive/Planet.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Planet : MonoBehaviour {
-
     [Range(2,256)]
     public int resolution = 10;
     public bool autoUpdate = true;
@@ -25,23 +24,19 @@ public class Planet : MonoBehaviour {
     MeshFilter[] meshFilters;
     TerrainFace[] terrainFaces;
      
-
 	void Initialize() {
         shapeGenerator.UpdateSettings(shapeSettings);
         colourGenerator.UpdateSettings(colourSettings);
 
-        if (meshFilters == null || meshFilters.Length == 0)
-        {
+        if (meshFilters == null || meshFilters.Length == 0) {
             meshFilters = new MeshFilter[6];
         }
         terrainFaces = new TerrainFace[6];
 
         Vector3[] directions = { Vector3.up, Vector3.down, Vector3.left, Vector3.right, Vector3.forward, Vector3.back };
 
-        for (int i = 0; i < 6; i++)
-        {
-            if (meshFilters[i] == null)
-            {
+        for (int i = 0; i < 6; i++) {
+            if (meshFilters[i] == null) {
                 GameObject meshObj = new GameObject("mesh");
                 meshObj.transform.parent = transform;
 
@@ -57,15 +52,13 @@ public class Planet : MonoBehaviour {
         }
     }
 
-    public void GeneratePlanet()
-    {
+    public void GeneratePlanet() {
         Initialize();
         GenerateMesh();
         GenerateColours();
     }
 
-    public void OnShapeSettingsUpdated()
-    {
+    public void OnShapeSettingsUpdated() {
         if (autoUpdate)
         {
             Initialize();
@@ -73,8 +66,7 @@ public class Planet : MonoBehaviour {
         }
     }
 
-    public void OnColourSettingsUpdated()
-    {
+    public void OnColourSettingsUpdated() {
         if (autoUpdate)
         {
             Initialize();
@@ -82,10 +74,8 @@ public class Planet : MonoBehaviour {
         }
     }
 
-    void GenerateMesh()
-    {
-        for (int i = 0; i < 6; i++)
-        {
+    void GenerateMesh() {
+        for (int i = 0; i < 6; i++) {
             if (meshFilters[i].gameObject.activeSelf)
             {
                 terrainFaces[i].ConstructMesh();
