90469741d2e9144fc6f8f8b9d470d9c1cf283470
Liam Arbuckle <liam@skinetics.tech>
2023-07-30 16:21:38 +0800

    🐡🎪 ↝ Updating planet turn speed from supabase session

--- immersive/Assets/Scripts/PlanetNameUIManager.cs
+++ immersive/Assets/Scripts/PlanetNameUIManager.cs
@@ -6,7 +6,14 @@ using UnityEngine.UI;
 public class PlanetNameUIManager : MonoBehaviour {
     public Text planetNameText;
 
+    public Planet planet; // Reference to the Planet script
+
     public void UpdatePlanetName(string newPlanetName) {
         planetNameText.text = newPlanetName;
+    }
+
+    public void UpdatePlanetTurnSpeed(float newPlanetTurnSpeed) {
+        // Update the TurnSpeed variable in the Planet script
+        planet.TurnSpeed = newPlanetTurnSpeed;
     }
 }
