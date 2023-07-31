154f32b554c9ccc3493f8ded8b1b61a7e634a090
Liam Arbuckle <liam@skinetics.tech>
2023-07-31 16:46:05 +0800

    🗾🥩 ↝ Planet stats now being updatedgit add .

---
+++ immersive/Assets/Scripts/UpdatePlanetRotateSpeed.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UpdatePlanetRotateSpeed : MonoBehaviour {
+    public Planet planet; // Reference to the Planet script
+
+    public void UpdatePlanetTurnSpeed(float newPlanetTurnSpeed) {
+        // Update the TurnSpeed variable in the Planet script
+        planet.TurnSpeed = newPlanetTurnSpeed;
+    }
+}
