8de8f5803125f4cf291af8dd33004cb54cb389b4
Liam Arbuckle <liam@skinetics.tech>
2023-09-20 12:19:37 +1000

    🍹✈️ ↝ New scripts to set strength manually in-editor

--- immersive/Assets/Scripts/UpdatePlanetRotateSpeed.cs
+++ immersive/Assets/Scripts/UpdatePlanetRotateSpeed.cs
@@ -5,9 +5,26 @@ using UnityEngine.UI;
 
 public class UpdatePlanetRotateSpeed : MonoBehaviour {
     public Planet planet; // Reference to the Planet script
+    public ColourSettings colourSettings; // Reference to that planet's colourSettings
 
     public void UpdatePlanetTurnSpeed(float newPlanetTurnSpeed) {
         // Update the TurnSpeed variable in the Planet script
         planet.TurnSpeed = newPlanetTurnSpeed;
+
+        // Access the ColourSettings of the planet and update noiseStrength
+        if (planet != null && planet.colourSettings != null) {
+            planet.colourSettings.biomeColourSettings.noiseStrength = 3.0f;
+        }
+    }
+
+    public void UpdatePlanetStrength(float newPlanetStrength) {
+        planet.colourSettings.biomeColourSettings.noiseStrength = newPlanetStrength;
+    }
+
+    public void SetNoiseStrengthTo3() {
+        // Access the ColourSettings of the planet and update noiseStrength
+        if (planet != null && planet.colourSettings != null) {
+            planet.colourSettings.biomeColourSettings.noiseStrength = 3.0f;
+        }
     }
 } 
