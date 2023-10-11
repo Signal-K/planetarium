c1e126acc2769fd8db7debb2efa37a75702c141a
Liam Arbuckle <liam@skinetics.tech>
2023-10-11 14:15:56 +1100

    💇🏻‍♀️🐖 ↝ Dotnet application test to interact with supabase cloud

---
+++ dotnet/Program.cs
@@ -0,0 +1,11 @@
+﻿using Supabase;
+
+var url = "https://qwbufbmxkjfaikoloudl.supabase.co";
+var key = "";
+
+var options = new Supabase.SupabaseOptions {
+    AutoConnectRealtime = true
+};
+
+var supabase = new Supabase.Client(url, key, options);
+await supabase.InitializeAsync();
